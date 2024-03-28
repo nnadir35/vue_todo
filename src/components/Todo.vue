39396e2afce1c7676bb5f2ef9b5806954f347d77
nnadir35 <nadiralkis35@gmail.com>
2024-03-28 09:12:42 +0300

    first commit

---
+++ src/components/Todo.vue
@@ -0,0 +1,28 @@
+<!-- MyChildComponent.vue -->
+<template>
+  <div class="q-pa-md example-column-flex-order">
+    <div class="row">
+      <div class="column">{{ todo.id }}</div>
+      <div class="column">
+        <div>{{ todo.task }}</div>
+        <div>{{ todo.responsible }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todo",
+  props: {
+    todo: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* İstediğiniz CSS stilleri */
+</style>
---
+++ src/pages/TodoPage.vue
@@ -0,0 +1,32 @@
+<!-- MyParentComponent.vue -->
+<template>
+  <div>
+    <div v-for="(item, index) in tasks" :key="index">
+      <Todo :todo="item" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Todo from "components/Todo.vue";
+
+export default {
+  name: "TodoPage",
+  components: {
+    Todo,
+  },
+  data() {
+    return {
+      tasks: [
+        { id: 1, task: "Vue.js öğrenmek", responsible: "Ahmet" },
+        { id: 2, task: "React uygulaması geliştirmek", responsible: "Ayşe" },
+        { id: 3, task: "Angular projesi tasarlamak", responsible: "Mehmet" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+/* İstediğiniz CSS stilleri */
+</style>
